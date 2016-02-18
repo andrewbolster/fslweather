d43ba735325ed064b086e24f59004187716abc5e
Andrew Bolster <me@andrewbolster.info>
2016-02-18 12:03:26 +0000

    Float formatting and service-flag

--- fslweather/__init__.py
+++ fslweather/__init__.py
@@ -24,14 +24,13 @@ def calculate_rainfall_value():
 def calculate_rainfall_rate():
     global count
     global start_time
-    return count/(time()-start_time) * 60 * calculate_rainfall_value() #mm/h
+    return count/(time()-start_time) * 3600 * calculate_rainfall_value() #mm/h
 
 # Custom Value and Rate generators for Windspeed
 r_cm = 9.0
 
 interval = 1.0
 calculate_interval = 5.0
-max_speed = 0.0
 
 def calculate_windspeed():
     global count
@@ -42,6 +41,12 @@ def calculate_windspeed():
     km_per_sec = d_km / calculate_interval
     km_per_hour = km_per_sec * 3600
     return round (km_per_hour, 3)
+
+def count_interval():
+    global count
+    this_count = count
+    count = 0
+    return this_count
 
 
 def action(channel):
@@ -57,34 +62,39 @@ def main():
     parser = ArgumentParser(prog='fslweather')
     parser.add_argument('--rainfall', action='store_true')
     parser.add_argument('--windspeed', action='store_true')
+    parser.add_argument('--asservice', action='store_true')
     args = parser.parse_args()
+
     if not (args.rainfall or args.windspeed):
         raise ValueError("Must Use Either --rainfall or --windspeed")
     if args.rainfall and args.windspeed:
         raise ValueError("Must use EITHER --rainfall or --windspeed")
 
     if args.rainfall:
-        template = "\rRainfall: {total}\t Est. Rate: {rate}"
+        template = "\rRainfall: {sum:6.3f} mm\t Est. Rate: {rate:6.3f} mm/hr"
         value = calculate_rainfall_value
         rate = calculate_rainfall_rate
     if args.windspeed:
-        template = '\rThe current speed is: %.2f km/h | Biggest so far: %.2f km/h'
-        value = calculate_windspeed()
+        template = '\rThe current speed is: {rate:} km/h | Biggest so far: {max} km/h'
+        value = count_interval
+        rate = calculate_windspeed
 
 
-    last_value = 0.0
+    last_max = 0.0
     while True:
-        v = value()
         r = rate()
+        v = value()
         data = {
-            'total' : v,
+            'sum' : v,
             'rate' :  r,
-            'max_rate' : max(r, last_value),
+            'max' : max(r, last_max),
             'last_action' : time()-last_time,
             'last_update' : time()
         }
+        last_max = data['max']
 
         with open(json_path, 'w') as outfile:
             json.dump(data, outfile)
-        print(template.format(**data), end='')
-        sleep(10)
+        if not args.asservice:
+            print(template.format(**data), end='')
+        sleep(calculate_interval)
